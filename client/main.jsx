d8102cbaa51bd4c1d0e6d02051fc6e2326221574
Thomas Gee <thomasrobertgee@gmail.com>
2017-01-29 21:04:50 +1100

    can now add items to the list via the web browser, up to date, beginning chap 5

--- client/main.jsx
+++ client/main.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 
-import App from './imports/ui/App.js'
-
-import '../imports/api/tasks.js';
+import App from '../imports/ui/App.jsx'
 
 Meteor.startup(() => {
   render(<App />, document.getElementById('render-target'));
